f414f063aee83c4b692339eb4987da884e002d9f
CodingBapthi <sebastiantittel@mail.de>
2024-02-11 20:35:14 +0100

    Add utils.ts, import global.css, and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      integrations: false,
+    }),
+    react(),
+  ],
 });
